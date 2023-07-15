72914efb15cef4f3bcaa441800d640440361e4cd
joaooliveira247 <joaooliveira247@gmail.com>
2023-07-14 21:46:18 -0300

    :book:

--- panic/src/main.rs
+++ panic/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs::File;
+use std::io::ErrorKind;
 
 #[allow(dead_code)]
 fn escopo_1() {
@@ -11,6 +12,8 @@ fn escopo_2() {
     v[99];
 }
 
+#[allow(dead_code)]
+#[allow(unused_variables)]
 fn escopo_3() {
     let f = File::open("hello.txt");
 
@@ -18,10 +21,45 @@ fn escopo_3() {
         Ok(file) => file,
         Err(error) => {
             panic!("Houve um problema ao abrir o arquivo: {:?}", error);
-        },
+        }
     };
 }
 
+#[allow(dead_code)]
+#[allow(unused_variables)]
+fn escopo_4() {
+    let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(ref error) if error.kind() == ErrorKind::NotFound => match File::create("hello.txt") {
+            Ok(fc) => fc,
+            Err(e) => {
+                panic!("Rust try create a file 'n' something happen in {:?}", e)
+            }
+        },
+        Err(error) => {
+            panic!("Houve um problema ao abrir o arquivo {:?}", error)
+        }
+    };
+}
+
+#[allow(dead_code)]
+#[allow(unused_variables)]
+fn escopo_5() {
+    let f = File::open("hello.txt").unwrap();
+}
+
+#[allow(dead_code)]
+#[allow(unused_variables)]
+fn escopo_6() {
+    let f = File::open("hello.txt").expect("Error to open hello.txt");
+}
+
+fn escopo_7() {
+    
+}
+
 fn main() {
-    escopo_3();
+    escopo_6();
 }

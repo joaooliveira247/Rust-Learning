8d93a3a5dee20205144a080f095560467d801e3a
joaooliveira247 <joaooliveira247@gmail.com>
2023-07-21 11:22:07 -0300

    :book:

---
+++ tests_results/src/lib.rs
@@ -0,0 +1,13 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two doesn't equal four"))
+        }
+    }
+}

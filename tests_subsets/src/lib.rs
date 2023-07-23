c7ede19563ce29f38c1ea00816fe2b050bbc049e
joaooliveira247 <joaooliveira247@gmail.com>
2023-07-23 14:06:57 -0300

    :book:

---
+++ tests_subsets/src/lib.rs
@@ -0,0 +1,25 @@
+pub fn add_two(num: usize) -> usize {
+    num + 2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn add_two_and_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn add_three_and_two() {
+        assert_eq!(5, add_two(3));
+    }
+
+    #[test]
+    #[ignore]
+    fn one_hundred() {
+        assert_eq!(102, add_two(100));
+    }
+
+}

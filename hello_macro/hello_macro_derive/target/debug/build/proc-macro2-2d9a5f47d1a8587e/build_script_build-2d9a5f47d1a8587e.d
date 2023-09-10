6122b91c162fa969a06265004c29695a5734aeaf
joaooliveira247 <joaooliveira247@gmail.com>
2023-09-10 15:23:08 -0300

    :book:

---
+++ hello_macro/hello_macro_derive/target/debug/build/proc-macro2-2d9a5f47d1a8587e/build_script_build-2d9a5f47d1a8587e.d
@@ -0,0 +1,5 @@
+/home/virusxdrj/Projects/Rust/Learning/hello_macro/hello_macro_derive/target/debug/build/proc-macro2-2d9a5f47d1a8587e/build_script_build-2d9a5f47d1a8587e: /home/virusxdrj/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.66/build.rs
+
+/home/virusxdrj/Projects/Rust/Learning/hello_macro/hello_macro_derive/target/debug/build/proc-macro2-2d9a5f47d1a8587e/build_script_build-2d9a5f47d1a8587e.d: /home/virusxdrj/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.66/build.rs
+
+/home/virusxdrj/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.66/build.rs:
---
+++ hello_macro/hello_macro_derive/target/debug/deps/unicode_ident-6b733c0f01eae78e.d
@@ -0,0 +1,6 @@
+/home/virusxdrj/Projects/Rust/Learning/hello_macro/hello_macro_derive/target/debug/deps/unicode_ident-6b733c0f01eae78e.rmeta: /home/virusxdrj/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.11/src/lib.rs /home/virusxdrj/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.11/src/tables.rs
+
+/home/virusxdrj/Projects/Rust/Learning/hello_macro/hello_macro_derive/target/debug/deps/unicode_ident-6b733c0f01eae78e.d: /home/virusxdrj/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.11/src/lib.rs /home/virusxdrj/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.11/src/tables.rs
+
+/home/virusxdrj/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.11/src/lib.rs:
+/home/virusxdrj/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.11/src/tables.rs:

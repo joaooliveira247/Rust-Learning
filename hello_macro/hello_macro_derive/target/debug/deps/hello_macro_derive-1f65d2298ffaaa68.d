8592d5980e759403692d49c451999de98f324be5
joaooliveira247 <joaooliveira247@gmail.com>
2023-09-10 15:25:28 -0300

    Revert ":book:"
    This reverts commit 6122b91c162fa969a06265004c29695a5734aeaf.

--- hello_macro/hello_macro_derive/target/debug/deps/hello_macro_derive-1f65d2298ffaaa68.d
+++
@@ -1,5 +0,0 @@
-/home/virusxdrj/Projects/Rust/Learning/hello_macro/hello_macro_derive/target/debug/deps/hello_macro_derive-1f65d2298ffaaa68.rmeta: src/lib.rs
-
-/home/virusxdrj/Projects/Rust/Learning/hello_macro/hello_macro_derive/target/debug/deps/hello_macro_derive-1f65d2298ffaaa68.d: src/lib.rs
-
-src/lib.rs:
--- hello_macro/hello_macro_derive/target/debug/deps/hello_macro_derive-e518db49dfbf0bf8.d
+++
@@ -1,5 +0,0 @@
-/home/virusxdrj/Projects/Rust/Learning/hello_macro/hello_macro_derive/target/debug/deps/hello_macro_derive-e518db49dfbf0bf8.rmeta: src/lib.rs
-
-/home/virusxdrj/Projects/Rust/Learning/hello_macro/hello_macro_derive/target/debug/deps/hello_macro_derive-e518db49dfbf0bf8.d: src/lib.rs
-
-src/lib.rs:
